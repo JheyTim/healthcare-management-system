d4c304e3fac0fc49237b804f57960b5b033c6f7c
Timothy Jhey Sarmiento <timothyjhey@gmail.com>
2024-10-10 17:30:25 +0800

    Implemented pagination

--- controllers/auditLogController.js
+++ controllers/auditLogController.js
@@ -1,7 +1,7 @@
 const AuditLog = require('../models/AuditLog');
 
 exports.getAllAuditLogs = async (req, res) => {
-  const { user, action, resource } = req.query;
+  const { user, action, resource, page = 1, limit = 10 } = req.query;
 
   let query = {};
 
@@ -18,8 +18,24 @@ exports.getAllAuditLogs = async (req, res) => {
   }
 
   try {
-    const logs = await AuditLog.find(query).populate('user');
-    res.json(logs);
+    const logsPromise = AuditLog.find(query)
+      .populate('user')
+      .skip((page - 1) * limit)
+      .limit(+limit);
+
+    const totalLogsPromise = AuditLog.countDocuments(query); // Total log count for pagination
+
+    const [logs, totalLogs] = await Promise.all([
+      logsPromise,
+      totalLogsPromise,
+    ]);
+
+    res.json({
+      logs,
+      totalLogs,
+      currentPage: +page,
+      totalPages: Math.ceil(totalLogs / limit),
+    });
   } catch (error) {
     res.status(500).json({ message: 'Error fetching audit logs', error });
   }
