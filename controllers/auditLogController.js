4dc9660472f6fdb30ba631ce487eb878b72c29bb
Timothy Jhey Sarmiento <timothyjhey@gmail.com>
2024-10-10 17:03:33 +0800

    Enhancing Audit Logs with Filtering and Search

--- controllers/auditLogController.js
+++ controllers/auditLogController.js
@@ -1,8 +1,24 @@
 const AuditLog = require('../models/AuditLog');
 
 exports.getAllAuditLogs = async (req, res) => {
+  const { user, action, resource } = req.query;
+
+  let query = {};
+
+  if (user) {
+    query.user = user;
+  }
+
+  if (action) {
+    query.action = action;
+  }
+
+  if (resource) {
+    query.resource = resource;
+  }
+
   try {
-    const logs = await AuditLog.find().populate('user');
+    const logs = await AuditLog.find(query).populate('user');
     res.json(logs);
   } catch (error) {
     res.status(500).json({ message: 'Error fetching audit logs', error });
